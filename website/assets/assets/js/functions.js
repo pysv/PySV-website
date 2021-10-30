52e93e977ea6b32c4ce885290d2561b65ecbecd2
Tereza Iofciu <tereza@neuefische.com>
2021-10-30 18:52:39 +0200

    fixed header and styling isssue

--- website/assets/assets/js/functions.js
+++ website/assets/assets/js/functions.js
@@ -565,9 +565,9 @@ var e = {
           var changeThemeToDark = () => {
             document.documentElement.setAttribute("data-theme", "dark") // set theme to dark
             if(dir == 'rtl') {
-                style.setAttribute('href', 'assets/css/style-dark-rtl.css');
+                style.setAttribute('href', '/assets/css/style-dark-rtl.css');
             } else {
-                style.setAttribute('href', 'assets/css/style-dark.css');
+                style.setAttribute('href', '/assets/css/style-dark.css');
             }
             localStorage.setItem("data-theme", "dark") // save theme to local storage
           }
@@ -575,9 +575,9 @@ var e = {
           var changeThemeToLight = () => {
             document.documentElement.setAttribute("data-theme", "light") // set theme light
             if(dir == 'rtl') {
-                style.setAttribute('href', 'assets/css/style-rtl.css');
+                style.setAttribute('href', '/assets/css/style-rtl.css');
             } else {
-                style.setAttribute('href', 'assets/css/style.css');
+                style.setAttribute('href', '/assets/css/style.css');
             }
             
             localStorage.setItem("data-theme", 'light') // save theme to local storage
